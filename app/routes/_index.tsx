c8c0d03568f4ff566f0587f2dff9d7202c98a0d8
Roger Stringer <roger@datamcfly.com>
2023-07-07 14:01:13 -0700

    put back

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { LoaderArgs } from '@vercel/remix';
-//import { useLoaderData } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import Button from '~/components/Button';
 import { cacheHeader } from 'pretty-cache-header';
 
